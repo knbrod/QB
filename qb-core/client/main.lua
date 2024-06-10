d6b24724b70271035307bdb43fc3c17121b014ef
Kelley Rodriguez <160456615+knbrod@users.noreply.github.com>
2024-06-10 10:38:26 -0400

    Add files via upload

---
+++ qb-core/client/main.lua
@@ -0,0 +1,10 @@
+QBCore = {}
+QBCore.PlayerData = {}
+QBCore.Config = QBConfig
+QBCore.Shared = QBShared
+QBCore.ClientCallbacks = {}
+QBCore.ServerCallbacks = {}
+
+exports('GetCoreObject', function()
+    return QBCore
+end)
---
+++ qb-core/server/main.lua
@@ -0,0 +1,9 @@
+QBCore = {}
+QBCore.Config = QBConfig
+QBCore.Shared = QBShared
+QBCore.ClientCallbacks = {}
+QBCore.ServerCallbacks = {}
+
+exports('GetCoreObject', function()
+    return QBCore
+end)
